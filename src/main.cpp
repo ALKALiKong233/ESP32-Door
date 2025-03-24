f94c5b232eb5604cb1f8ce32619284384ca33cca
ALKALiKong <alikongalk@gmail.com>
2025-03-24 19:47:59 +0800

    main: Provide a web API to open door
    * Also change the delay time of closing door to 5 seconds

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <WebServer.h>
+
 #include "servo-utils.h"
 #include "wifi-utils.h"
 #include "door.h"
@@ -6,11 +8,19 @@
 #define MG996R_PIN 13
 #define BUTTON 2
 
+#define WEB_SERVER_PORT 80
+
 volatile bool shouldOpen = false;
 
 void button_trigger() {
   shouldOpen = true;
 }
+
+void web_trigger() {
+  shouldOpen = true;
+}
+
+WebServer server(WEB_SERVER_PORT);
 
 void setup() {
   Serial.begin(9600);
@@ -18,12 +28,16 @@ void setup() {
   servo::mg996r::getInstance(MG996R_PIN, 0);
   pinMode(BUTTON, INPUT_PULLDOWN);
   attachInterrupt(digitalPinToInterrupt(BUTTON), button_trigger, FALLING);
+  server.on("/open", web_trigger);
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
+  server.handleClient();
   if ( shouldOpen ) {
     door::open();
-    delay(1000);
+    delay(5000);
     door::close();
     shouldOpen = false;
   }
