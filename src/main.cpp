a2ba324020ea4a090bb02dd017d3d8f1089291b3
ALKALiKong <alikongalk@gmail.com>
2025-03-24 17:28:10 +0800

    main: Add interrupt from external button

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 #include <Arduino.h>
 #include "servo-utils.h"
 #include "wifi-utils.h"
+#include "door.h"
 
 #define MG996R_PIN 13
+#define BUTTON 2
+
+volatile bool shouldOpen = false;
+
+void button_trigger() {
+  shouldOpen = true;
+}
 
 void setup() {
   Serial.begin(9600);
   setupWiFiConnection();
   servo::mg996r::getInstance(MG996R_PIN, 0);
+  pinMode(BUTTON, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(BUTTON), button_trigger, FALLING);
 }
 
 void loop() {
+  if ( shouldOpen ) {
+    door::open();
+    delay(1000);
+    door::close();
+    shouldOpen = false;
+  }
 }
